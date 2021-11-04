8cce5f2c31a198b61ba9641b968bcc682253b688
SamuelHFS <samuel.jpg1@gmail.com>
2021-11-04 09:06:40 -0300

    nao funcionando

--- pjSQLiteOfl/pjSQLiteOfl/pjSQLiteOfl/MainPage.xaml.cs
+++ pjSQLiteOfl/pjSQLiteOfl/pjSQLiteOfl/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SQLite;
+﻿using pjSQLiteOfl.model;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -15,46 +16,10 @@ namespace pjSQLiteOfl
     
     public partial class MainPage : ContentPage
     {
-        public class Livro : INotifyPropertyChanged
-
-        {
-            
-
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            
-
-            private string _titulo;
-
-            [MaxLength(150)]
-            public string Titulo
-            {
-                get
-                {
-                    return _titulo;
-                }
-                set
-                {
-                    if (_titulo == value)
-                        return;
-
-                    _titulo = value;
-
-                    OnPropertyChanged(nameof(Titulo));
-                }
-            }
-
-            private void OnPropertyChanged(string propertyName)
-            {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-
-        }
-
+      
         private SQLiteAsyncConnection _dbContext;
-        private Livro usuarioSelecionado;
-        private ObservableCollection<Livro> _usuarios;
+        private Livro livroSelecionado;
+        private ObservableCollection<Livro> _livros;
         private Boolean toque = false;
 
         public MainPage()
@@ -73,42 +38,48 @@ namespace pjSQLiteOfl
         protected async override void OnAppearing()
         {
             await _dbContext.CreateTableAsync<Livro>();
-            var usuarios = await _dbContext.Table<Livro>().ToListAsync();
-            _usuarios = new ObservableCollection<Livro>(usuarios);
-            lstvUsuarios.ItemsSource = _usuarios;
+            var livros = await _dbContext.Table<Livro>().ToListAsync();
+            _livros = new ObservableCollection<Livro>(livros);
+            lstvUsuarios.ItemsSource = _livros;
 
             base.OnAppearing();
         }
 
-        private async void IncluirClicked(object sender, System.EventArgs e)
+        private async Task IncluirClickedAsync(object sender, EventArgs e)
         {
             toque = false;
             Livro livro = new Livro();
-            livro.titulo = txtTitulo.Text.Trim();
+            livro.Titulo = txtTitulo.Text.Trim();
+            livro.Autor = txtTitulo.Text.Trim();
+            livro.Editora = txtEditora.Text.Trim();
+            livro.Ano = txtAno.Text.Trim();
 
-            if (!string.IsNullOrEmpty(txtNome.Text))
+            if(!string.IsNullOrEmpty(txtTitulo.Text))
             {
-                await _dbContext.InsertAsync(usuario);
-                _usuarios.Add(usuario);
-                txtNome.Text = "";
+                await _dbContext.InsertAsync(livro);
+                _livros.Add(livro);
+                txtTitulo.Text = "";
+                txtAutor.Text = "";
+                txtEditora.Text = "";
+                txtAno.Text = "";
             }
             else
             {
-                await DisplayAlert("Aviso", "Preencha o campo Nome", "Ok");
-                txtNome.Focus();
+                await DisplayAlert("Aviso", "Preencha os campos", "OK");
+                txtTitulo.Focus();
             }
 
         }
 
-        private async void AlterarClicked(object sender, System.EventArgs e)
+        private async Task AlterarClickedAsync(object sender, EventArgs e)
         {
-            if (toque == true)
+            if(toque == true)
             {
-                usuarioSelecionado.Nome = txtNome.Text.Trim();
-                if (!string.IsNullOrEmpty(txtNome.Text))
+                livroSelecionado.Titulo = txtTitulo.Text.Trim();
+                if(!string.IsNullOrEmpty(txtTitulo.Text))
                 {
-                    await _dbContext.UpdateAsync(usuarioSelecionado);
-                    txtNome.Text = "";
+                    await _dbContext.UpdateAsync(livroSelecionado);
+                    txtTitulo.Text = "";
                     toque = false;
                 }
                 else
@@ -116,24 +87,24 @@ namespace pjSQLiteOfl
                     await DisplayAlert("Aviso", "Preencha o campo Nome", "Ok");
                     toque = false;
                 }
-
             }
             else
             {
-                await DisplayAlert("Aviso", "Selecione um nome da lista", "Ok");
-            }
+                await DisplayAlert("Aviso", "Selecione  um nome da lista", "OK");
 
+            }
         }
 
-        private async void ExcluirClicked(object sender, System.EventArgs e)
+        private async Task ExcluirClickedAsync(object sender, EventArgs e)
         {
+
             if (toque == true)
             {
-                if (!string.IsNullOrEmpty(txtNome.Text))
+                if (!string.IsNullOrEmpty(txtTitulo.Text))
                 {
-                    await _dbContext.DeleteAsync(usuarioSelecionado);
-                    _usuarios.Remove(usuarioSelecionado);
-                    txtNome.Text = "";
+                    await _dbContext.DeleteAsync(livroSelecionado);
+                    _livros.Remove(livroSelecionado);
+                    txtTitulo.Text = "";
                     toque = false;
                 }
                 else
@@ -146,170 +117,13 @@ namespace pjSQLiteOfl
             {
                 await DisplayAlert("Aviso", "Selecione um nome da lista", "Ok");
             }
-
         }
 
         private void lstvUsuarios_ItemTapped(object sender, ItemTappedEventArgs e)
         {
             toque = true;
-            usuarioSelecionado = e.Item as Usuario;
-            txtNome.Text = usuarioSelecionado.Nome;
-
-        }
-    }
-}
-using SQLite;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace SQLite01
-{
-    public partial class MainPage : ContentPage
-    {
-        public class Usuario : INotifyPropertyChanged
-        {
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            [PrimaryKey, AutoIncrement]
-            public int Id { get; set; }
-
-            private string _nome;
-
-            [MaxLength(150)]
-            public string Nome
-            {
-                get
-                {
-                    return _nome;
-                }
-                set
-                {
-                    if (_nome == value)
-                        return;
-
-                    _nome = value;
-
-                    OnPropertyChanged(nameof(Nome));
-                }
-            }
-
-            private void OnPropertyChanged(string propertyName)
-            {
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-
-        }
-
-        private SQLiteAsyncConnection _dbContext;
-        private Usuario usuarioSelecionado;
-        private ObservableCollection<Usuario> _usuarios;
-        private Boolean toque = false;
-
-        public MainPage()
-        {
-            InitializeComponent();
-            _dbContext = DependencyService.Get<IAcessoDB>().GetConnection();
-        }
-
-        //private void lstvUsuarios_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        //{
-        //    toque = true;
-        //    usuarioSelecionado = e.SelectedItem as Usuario;
-        //    txtNome.Text = usuarioSelecionado.Nome;
-        //}
-
-        protected async override void OnAppearing()
-        {
-            await _dbContext.CreateTableAsync<Usuario>();
-            var usuarios = await _dbContext.Table<Usuario>().ToListAsync();
-            _usuarios = new ObservableCollection<Usuario>(usuarios);
-            lstvUsuarios.ItemsSource = _usuarios;
-
-            base.OnAppearing();
-        }
-
-        private async void IncluirClicked(object sender, System.EventArgs e)
-        {
-            toque = false;
-            Usuario usuario = new Usuario();
-            usuario.Nome = txtNome.Text.Trim();
-
-            if (!string.IsNullOrEmpty(txtNome.Text))
-            {
-                await _dbContext.InsertAsync(usuario);
-                _usuarios.Add(usuario);
-                txtNome.Text = "";
-            }
-            else
-            {
-                await DisplayAlert("Aviso", "Preencha o campo Nome", "Ok");
-                txtNome.Focus();
-            }
-
-        }
-
-        private async void AlterarClicked(object sender, System.EventArgs e)
-        {
-            if (toque == true)
-            {
-                usuarioSelecionado.Nome = txtNome.Text.Trim();
-                if (!string.IsNullOrEmpty(txtNome.Text))
-                {
-                    await _dbContext.UpdateAsync(usuarioSelecionado);
-                    txtNome.Text = "";
-                    toque = false;
-                }
-                else
-                {
-                    await DisplayAlert("Aviso", "Preencha o campo Nome", "Ok");
-                    toque = false;
-                }
-
-            }
-            else
-            {
-                await DisplayAlert("Aviso", "Selecione um nome da lista", "Ok");
-            }
-
-        }
-
-        private async void ExcluirClicked(object sender, System.EventArgs e)
-        {
-            if (toque == true)
-            {
-                if (!string.IsNullOrEmpty(txtNome.Text))
-                {
-                    await _dbContext.DeleteAsync(usuarioSelecionado);
-                    _usuarios.Remove(usuarioSelecionado);
-                    txtNome.Text = "";
-                    toque = false;
-                }
-                else
-                {
-                    await DisplayAlert("Aviso", "Selecione um nome da lista", "Ok");
-                    toque = false;
-                }
-            }
-            else
-            {
-                await DisplayAlert("Aviso", "Selecione um nome da lista", "Ok");
-            }
-
-        }
-
-        private void lstvUsuarios_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
-            toque = true;
-            usuarioSelecionado = e.Item as Usuario;
-            txtNome.Text = usuarioSelecionado.Nome;
-
+            livroSelecionado = e.Item as Livro;
+            txtTitulo.Text = livroSelecionado.Titulo;
         }
     }
 }
--- pjSQLiteOfl/pjSQLiteOfl/pjSQLiteOfl/model/Livro.cs
+++ pjSQLiteOfl/pjSQLiteOfl/pjSQLiteOfl/model/Livro.cs
@@ -1,21 +1,118 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace pjSQLiteOfl.model
 {
     class Livro
     {
-        [PrimaryKey, AutoIncrement]
-        public int Id { get; set; }
+       
+            public event PropertyChangedEventHandler PropertyChanged;
 
-        public string titulo { get; set; }
+        private int _id;
+            [PrimaryKey, AutoIncrement]
 
-        public string autor { get; set; }
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (_id == value)
+                    return;
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
 
-        public string editora { get; set; }
 
-        public string ano { get; set; }
+            private string _titulo;
+
+            [MaxLength(150)]
+            public string Titulo
+            {
+                get
+                {
+                    return _titulo;
+                }
+                set
+                {
+                    if (_titulo == value)
+                        return;
+
+                    _titulo = value;
+
+                    OnPropertyChanged(nameof(Titulo));
+                }
+            }
+            private string _autor;
+            [MaxLength(300)]
+            public string Autor
+            {
+                get
+                {
+                    return _autor;
+                }
+                set
+                {
+                    if (_autor == value)
+                        return;
+
+                    _titulo = value;
+                    OnPropertyChanged(nameof(Autor));
+                }
+            }
+
+            private string _editora;
+            [MaxLength(200)]
+
+            public string Editora
+            {
+                get
+                {
+                    return _editora;
+
+                }
+                set
+                {
+                    if (_editora == value)
+                        return;
+
+                    _editora = value;
+                    OnPropertyChanged(nameof(Editora));
+                }
+            }
+
+            private string _ano;
+            [MaxLength(100)]
+
+            public string Ano
+            {
+                get
+                {
+                    return _ano;
+                }
+                set
+                {
+                    if (_ano == value)
+                        return;
+
+                    _ano = value;
+                    OnPropertyChanged(nameof(Ano));
+                }
+            }
+
+            private void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+
     }
-}
+
